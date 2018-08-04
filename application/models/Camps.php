472019435489849bfe7ca0c3c0cc1c80a2505b5a
Subin Siby <subins2000@gmail.com>
2018-08-04 23:54:11 +0530

    Camps management

--- application/models/Camps.php
+++ application/models/Camps.php
@@ -9,18 +9,18 @@
                 parent::__construct();
             }
 
-             function add_camp($lat, $lng, $uid) {
+             function add_camp($lat, $lng, $title, $people, $uid) {
                 $this->lat = $lat;
                 $this->lng = $lng;
+                $this->title = $title;
+                $this->people = $people;
                 $this->uid = $uid;
                 $this->db->insert('camps', $this);
               }
 
-              function get_user($name, $password)  {
+              function get_all()  {
                 $this->db->select('*');
                 $this->db->from('camps');
-                $this->db->where('name', $name);
-                $this->db->where('password', $password);
                 $query = $this->db->get();
 
                 return $query->result();
