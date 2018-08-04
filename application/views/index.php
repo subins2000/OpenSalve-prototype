ed4746030eaf8a7e5f1444c778aa59df47402783
unknown <geongeorge98@gmail.com>
2018-08-04 15:52:49 +0530

    changed btstrp and added a navbar

--- application/views/index.php
+++ application/views/index.php
@@ -5,6 +5,37 @@
 		<link rel="stylesheet" href="<?=asset_url(); ?>css/bootstrap.min.css">
 	</head>
 	<body>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+		<div class="container">
+			Hello
+		</div>
 
 
 	<script src="<?=asset_url(); ?>js/jquery.js"></script>
