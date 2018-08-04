b2d9033697c44ee39d4b3f81519b169075ee3ffc
Subin Siby <subins2000@gmail.com>
2018-08-04 15:32:45 +0530

    Fix bugs

--- application/views/index.php
+++ application/views/index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<link href="" rel="stylesheet" />
+		<link rel="stylesheet" href="/static/css/main.css" />
 		<link rel="stylesheet" href="/static/css/bootstrap.min.css">
 
 		<script src="/static/js/jquery.js"></script>
 		<script src="/static/js/bootstrap.js"></script>
 	</head>
 	<body>
-		
+
 	</body>
 </html>
