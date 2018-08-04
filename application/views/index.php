08502ba567fad1e6735e164255a0f71844a08d98
Subin Siby <subins2000@gmail.com>
2018-08-04 15:20:53 +0530

    Initial commit

---
+++ application/views/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<link href="" rel="stylesheet" />
+		<link rel="stylesheet" href="/static/css/bootstrap.min.css">
+		<script src="/static/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+
+	</body>
+</html>
