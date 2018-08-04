c7aa934b7609a5b719667e5ef6ec776d3209b72f
unknown <geongeorge98@gmail.com>
2018-08-04 15:42:26 +0530

    Added asset_url() helper

--- application/views/index.php
+++ application/views/index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<link rel="stylesheet" href="/static/css/main.css" />
-		<link rel="stylesheet" href="/static/css/bootstrap.min.css">
-
-		<script src="/static/js/jquery.js"></script>
-		<script src="/static/js/bootstrap.js"></script>
+		<link rel="stylesheet" href="<?=asset_url(); ?>css/main.css" />
+		<link rel="stylesheet" href="<?=asset_url(); ?>css/bootstrap.min.css">
 	</head>
 	<body>
 
+
+	<script src="<?=asset_url(); ?>js/jquery.js"></script>
+	<script src="<?=asset_url(); ?>js/bootstrap.js"></script>
 	</body>
 </html>
