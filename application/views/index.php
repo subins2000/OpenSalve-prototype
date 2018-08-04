b0799344d4f865203791dbef703aaa629adb15ae
Subin Siby <subins2000@gmail.com>
2018-08-04 22:08:29 +0530

    Change index

--- application/views/index.php
+++ application/views/index.php
@@ -1,56 +1,64 @@
 <div class="container">
 	<center id="brand-title">
 		<h1 style="margin-top: 0;">OpenSalve</h1>
-		<p>Lending a hand</p>
+		<p>Helping relief efforts</p>
 	</center>
-	<div class="card-columns">
-		<div class="card stat" style="width: 18rem;">
-	  		<div class="card-body">
-				<h2>85</h2>
-				<span>Camps</span>
-			</div>
-		</div>
-		<div class="card stat" style="width: 18rem;">
-	  		<div class="card-body">
-				<h2>₹53.4L</h2>
-				<span>Relief fund dispersed</span>
-			</div>
-		</div>
-		<div class="card stat" style="width: 18rem;">
-	  		<div class="card-body">
-				<h2></h2>
-				<span></span>
-			</div>
-		</div>
-	</div>
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDuWSldX-non0YF-dn35hbE4BcIQoqwDDk&libraries=visualization">
+	</script>
+	<div id="map" style="height: 600px;width: 100%;"></div>
+	<script>
+		function getRandomInt(min, max) {
+			return Math.floor(Math.random() * (max - min + 1)) + min;
+		}
+
+		/* Data points defined as an array of LatLng objects */
+		var heatmapData = [
+			new google.maps.LatLng(9.481575, 76.387940),
+			new google.maps.LatLng(9.480248, 76.387018),
+			new google.maps.LatLng(9.480248, 76.387018),
+			new google.maps.LatLng(9.480148, 76.385118),
+			new google.maps.LatLng(9.480148, 76.388118),
+			new google.maps.LatLng(9.482148, 76.388118),
+		];
+
+		map = new google.maps.Map(document.getElementById('map'), {
+			center: {
+				lat: 9.481618,
+				lng: 76.387309,
+			},
+			zoom: 17,
+		});
+
+		var heatmap = new google.maps.visualization.HeatmapLayer({
+			data: heatmapData,
+			radius: 45,
+			opacity: 1,
+		});
+
+		heatmap.setMap(map);
+	</script>
 </div>
-<!--
 <div class="bg-black">
-	<div class="container">
-		<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDuWSldX-non0YF-dn35hbE4BcIQoqwDDk&libraries=visualization">
-		</script>
-		<div id="map" style="height: 600px;width: 100%;"></div>
-		<script>
-			/* Data points defined as an array of LatLng objects */
-			var heatmapData = [
-				new google.maps.LatLng(9.481575, 76.387940),
-				new google.maps.LatLng(9.480248, 76.387018),
-			];
-
-			map = new google.maps.Map(document.getElementById('map'), {
-			    center: {
-			        lat: 9.481618,
-			        lng: 76.387309,
-			    },
-			    zoom: 17,
-			});
-
-			var heatmap = new google.maps.visualization.HeatmapLayer({
-			    data: heatmapData,
-			    radius: 20,
-			    opacity: 1,
-			});
-		</script>
+	<div class="container" id="stat-container">
+		<div class="card-columns">
+			<div class="card stat" style="width: 18rem;">
+		  		<div class="card-body">
+					<h2>85</h2>
+					<span>Camps</span>
+				</div>
+			</div>
+			<div class="card stat" style="width: 18rem;">
+		  		<div class="card-body">
+					<h2>₹53.4L</h2>
+					<span>Relief fund dispersed</span>
+				</div>
+			</div>
+			<div class="card stat" style="width: 18rem;">
+		  		<div class="card-body">
+					<h2>243</h2>
+					<span>Volunteers</span>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
--->
