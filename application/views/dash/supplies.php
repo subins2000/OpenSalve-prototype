3c14476814d677c35a21914e24488963244682dd
Subin Siby <subins2000@gmail.com>
2018-08-05 07:26:49 +0530

    Add chart

--- application/views/dash/supplies.php
+++ application/views/dash/supplies.php
@@ -45,5 +45,45 @@
                 <input type="text" id="campID" name="campID" class="form-control input-default">
             </div>
         </div>
+
+        <div class="card-title">
+            <h3>Admin Panel</h3>
+            <h5></h5>
+        </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
+        <canvas id="myChart" width="400" height="100"></canvas>
+        <script>
+        var ctx = document.getElementById("myChart").getContext('2d');
+        var data = {
+            labels: ["Water Bottles", "Rice", "Eggs"],
+            datasets: [
+                {
+                    label: "Required",
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    data: [150,100,210]
+                },
+                {
+                    label: "Current",
+                    backgroundColor: 'rgba(255, 206, 86, 0.2)',
+                    data: [50, 47, 87]
+                },
+            ]
+        };
+
+        var myBarChart = new Chart(ctx, {
+            type: 'bar',
+            data: data,
+            options: {
+                barValueSpacing: 40,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            min: 0,
+                        }
+                    }]
+                }
+            }
+        });
+        </script>
     </div>
 </div>
