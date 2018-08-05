ef591bd5e6b0a7ecaf77443e05fb3b44fc142754
unknown <geongeorge98@gmail.com>
2018-08-05 07:27:27 +0530

    ever

--- application/views/newindex.php
+++ application/views/newindex.php
@@ -142,5 +142,32 @@ map.on('load', function() {
             ]
         }
     }, 'waterway-label');
+
+
+    map.addLayer({
+        "id": "places",
+        "type": "symbol",
+        "source": {
+            "type": "geojson",
+            "data": {
+                "type": "FeatureCollection",
+                "features": [{
+                    "type": "Feature",
+                    "properties": {
+                        "description": "<strong>Make it Mount Pleasant</strong><p><a href=\"http://www.mtpleasantdc.com/makeitmtpleasant\" target=\"_blank\" title=\"Opens in a new window\">Make it Mount Pleasant</a> is a handmade and vintage market and afternoon of live entertainment and kids activities. 12:00-6:00 p.m.</p>",
+                        "icon": "theatre"
+                    },
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [76.387309, 9.481618]
+                    }
+                }]
+            }
+        },
+        "layout": {
+            "icon-image": "{icon}-15",
+            "icon-allow-overlap": true
+        }
+    });
 });
 </script>
